34e1416370c0fd4761e765bb9ff718e7fc54a1f1
Chuck D'Antonio <c_dantonio@acm.org>
2017-05-30 08:25:33 -0400

    Added security support and sections for webinars and videos

--- src/main/java/io/spring/pivotal/workshop/ui/controllers/WorkshopUIController.java
+++ src/main/java/io/spring/pivotal/workshop/ui/controllers/WorkshopUIController.java
@@ -13,40 +13,43 @@ import io.spring.pivotal.workshop.ui.services.WorkshopService;
 
 @Controller
 public class WorkshopUIController {
-	
+
 	@Autowired
 	WorkshopService workshopService;
-		
+
 	HashMap<String, Session> workshopSessions = new HashMap<String, Session>();
 
 	private final static String INDEX="index";
 	private final static String DAILY_CONTENT="dailyContent";
 	private final static String REQUIREMENTS="requirements";
 	private final static String ENABLEMENT_CONTENT="enablementContent";
-	
+    private final static String SCHEDULE="schedule";
+    private final static String WEBINARS="webinars";
+    private final static String LOGIN="login";
+
 	@RequestMapping("/")
 	public String home(Model model) {
 
-		model.addAttribute("workshop", workshopService.workshop());	
-		model.addAttribute("instructors", workshopService.instructors());	
-		model.addAttribute("workshopSessions", workshopService.workshopSessions());	
-		
+		model.addAttribute("workshop", workshopService.workshop());
+		model.addAttribute("instructors", workshopService.instructors());
+		model.addAttribute("workshopSessions", workshopService.workshopSessions());
+
 		return INDEX;
 	}
 
 	@RequestMapping("/dailyContent")
 	public String dailyContent(@RequestParam(value="id", defaultValue="index") String id, Model model) {
-			
-		model.addAttribute("workshopSessions", workshopService.workshopSessions());	
+
+		model.addAttribute("workshopSessions", workshopService.workshopSessions());
 		model.addAttribute("dailyContent", workshopService.dailyContent(id));
 		model.addAttribute("resources", workshopService.resources());
-		
+
 		return DAILY_CONTENT;
-	}	
-	
+	}
+
 	@RequestMapping("/requirements")
-	public String requirements(Model model) {		
-		model.addAttribute("workshopSessions", workshopService.workshopSessions());	
+	public String requirements(Model model) {
+		model.addAttribute("workshopSessions", workshopService.workshopSessions());
 		return REQUIREMENTS;
 	}
 
@@ -55,4 +58,19 @@ public class WorkshopUIController {
 		model.addAttribute("enablementContent", workshopService.content());
 		return ENABLEMENT_CONTENT;
 	}
+
+    @RequestMapping("/webinars")
+    public String webinars(Model model) {
+        return WEBINARS;
+    }
+
+    @RequestMapping("/schedule")
+    public String schedule(Model model) {
+        return SCHEDULE;
+    }
+
+    @RequestMapping("/login")
+    public String login(Model model) {
+        return LOGIN;
+    }
 }
